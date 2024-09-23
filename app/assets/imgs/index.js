c4bdad6b6314195b394af3f94ac96c7833712695
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-09-23 17:19:53 -0300

    cleanup: adicionado um arquivo de import no imgs e no components

---
+++ app/assets/imgs/index.js
@@ -0,0 +1,5 @@
+export { default as Apple } from "./apple.png";
+export { default as Facebook } from "./facebook.png";
+export { default as Google } from "./google.png";
+export { default as Logo } from "./Logo.png";
+export { default as Folha } from "./Folha.svg";
