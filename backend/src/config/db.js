73e1016a932b3b298b65c36b156f29b919b38ac8
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-09-30 18:43:00 -0300

    fix: melhor estruturado o config do db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,18 +1,34 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-	host: "db",
-	user: "user",
-	password: "FaunaAbc.1234",
-	database: "faunaABC"
-});
+let connection;
 
-connection.connect((err) => {
-	if (err) {
-		console.error("Erro ao conectar ao MySQL:", err);
-		return;
-	}
-	console.log("Conectado ao MySQL com sucesso!");
-});
+function connectToDatabase() {
+	connection = mysql.createConnection({
+		host: "db",
+		user: "user",
+		password: "FaunaAbc.1234",
+		database: "faunaABC",
+	});
+
+	connection.connect((err) => {
+		if (err) {
+			console.error("Erro ao conectar ao MySQL:", err);
+			console.log("Tentando reconectar em 5 segundos...");
+			setTimeout(connectToDatabase, 5000);
+		} else {
+			console.log("Conectado ao MySQL com sucesso!");
+		}
+	});
+
+	connection.on("error", (err) => {
+		console.error("Erro de conexão com MySQL", err);
+		if (err.code == "PROTOCOL_CONNECTION_LOST") {
+			console.log("Conexão perdida, tentando reconectar...");
+			connectToDatabase();
+		}
+	});
+}
+
+connectToDatabase();
 
 module.exports = connection;
