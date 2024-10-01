e900b82c5488df7443068f496b2fe6c76f11fb2e
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-10-01 11:01:53 -0300

    refactor: mudança na log

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
-	console.log(`Servidor rodando na porta http://192.168.0.125${PORT}`);
+	console.log(`Servidor rodando na porta http://192.168.0.125:${PORT}`);
 });
