5462d3b32aa750b871b218fc8c58f72c11323d5f
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-10-07 11:25:57 -0300

    feat: adicionado controller no Indexador

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,7 +1,9 @@
 const createCadastroUser = require("./user/addUser");
 const createCadastroBio = require("./profissional/addProfissional");
+const createCadastroOng = require("./ong/addOng");
 
 module.exports = {
 	createCadastroUser,
 	createCadastroBio,
+	createCadastroOng,
 };
