1115a0998787d4e8b7e06d01146afcb4ab3489d2
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-10-07 11:27:18 -0300

    feat: adicionado a rota do addOng

--- backend/src/routes/useRoutes.js
+++ backend/src/routes/useRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createCadastroUser, createCadastroBio } = require("../controllers");
+const { createCadastroUser, createCadastroBio, createCadastroOng } = require("../controllers");
 
 router.post("/addUser", createCadastroUser);
 router.post("/addBio", createCadastroBio);
+router.post("/addOng", createCadastroOng);
 
 module.exports = router;
