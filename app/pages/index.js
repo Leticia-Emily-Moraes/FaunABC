4264f730d73c35ca1e250501275d20c78edd1a4d
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-10-09 13:10:35 -0300

    feat: criado o indexador de paginas

---
+++ app/pages/index.js
@@ -0,0 +1,22 @@
+export { default as AjudasRapidas } from "./ajudasRapidas";
+export { default as CadastroDadosEmergenciasUser } from "./cadastroDadosEmergencia";
+export { default as CadastroDadosOng } from "./cadastroDadosONG";
+export { default as CadastroDadosPessoaisUser } from "./cadastroDadosPessoaisUser";
+export { default as CadastroDadosProfissionais } from "./cadastroDadosProfissionais";
+export { default as CadastroDadosUser } from "./cadastroDadosUser";
+export { default as CadastroEndereco } from "./cadastroEndereco";
+export { default as TelefonesOffline } from "./callOffline";
+export { default as ConfirmacaoDeCadastro } from "./confirmacaoCadastro";
+export { default as Default } from "./defaultApp";
+export { default as DefaultOffline } from "./defaultOff";
+export { default as EscolhaPerfil } from "./escolhaPerfil";
+export { default as Home } from "./home";
+export { default as Login } from "./paginaLogin";
+export { default as PerfilOng } from "./perfilOng";
+export { default as PerfilPessoal } from "./perfilPessoal";
+export { default as PerfilProfissional } from "./perfilProfissional";
+export { default as PerfilUsuario } from "./perfilUsuario";
+export { default as PrimeiraPagina } from "./primeiraPagina";
+export { default as RedefinirSenha } from "./redefinirSenha";
+export { default as VerificacaoDuasEtapas } from "./verificacaoDuasEtapas";
+
