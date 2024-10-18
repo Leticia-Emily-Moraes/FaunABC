545ce5554d4a54d773c3d9b70a4c6014cc4f6bd5
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-10-17 23:35:04 -0300

    feat: adicionado a api de verificação de dois fatores

---
+++ app/service/api/apiVerificacaoDoCodigo.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export const VerificaCodigo = async (email,codigoInserido) => {
+	try {
+		const response = await axios.post(
+			"http://192.168.200.156:3001/api/verificaCodigo",
+			{
+                email,
+                codigoInserido,
+            },
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+
+		console.log("Dados enviados com sucesso:", response.data);
+		return response.data;
+	} catch (error) {
+		if (error.response) {
+			console.error("Erro na resposta da API:", error.response.data);
+		} else if (error.request) {
+			console.error("Nenhuma resposta da API:", error.request);
+		} else {
+			console.error("Erro na configuração da requisição:", error.message);
+		}
+		throw error;
+	}
+};
