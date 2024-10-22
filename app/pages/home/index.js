e7b6fdf72076c170c4c886c4904c1cdb3d49567d
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-10-22 13:52:27 -0300

    feat: pagina home componentizada

--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -1,43 +1,36 @@
 import React from "react";
-import {
-	ContainerPP,
-	ContainerConteudo,
-	ContainerConteudo1,
-	ContainerConteudo2,
-	ButtonConteudo,
-	ButtonConteudoGrande,
-	ViewScroll,
-	Title,
-	Div,
-} from "./style";
+import { Container, ContainerComDoisBlocos, ContainerScroll } from "./style";
+import { ContainerComClick } from "../../components";
 import "react-native-gesture-handler";
 
 function Home() {
 	return (
-		<ContainerPP>
-			<ViewScroll>
-				<ContainerConteudo>
-					<ContainerConteudo1>
-						<Div>
-							<Title>Sua área</Title>
-							<ButtonConteudo />
-						</Div>
-						<Div>
-							<Title>Ocorrências</Title>
-							<ButtonConteudo />
-						</Div>
-					</ContainerConteudo1>
-					<ButtonConteudoGrande />
-					<ButtonConteudoGrande />
-					<ButtonConteudoGrande />
-				</ContainerConteudo>
-				<ContainerConteudo2>
-					<ButtonConteudoGrande />
-					<ButtonConteudoGrande />
-					<ButtonConteudoGrande />
-				</ContainerConteudo2>
-			</ViewScroll>
-		</ContainerPP>
+		<ContainerScroll>
+			<Container>
+				<ContainerComDoisBlocos>
+					<ContainerComClick
+						altura={"200px"}
+						largura={"50%"}
+						titulo={"sua área"}
+					></ContainerComClick>
+					<ContainerComClick
+						altura={"200px"}
+						largura={"50%"}
+						titulo={"Ocorrências"}
+					></ContainerComClick>
+				</ContainerComDoisBlocos>
+				<ContainerComClick
+					altura={"200px"}
+					largura={"100%"}
+					titulo={"chat"}
+				></ContainerComClick>
+				<ContainerComClick
+					altura={"200px"}
+					largura={"100%"}
+					titulo={"animais"}
+				></ContainerComClick>
+			</Container>
+		</ContainerScroll>
 	);
 }
 
--- app/pages/home/style.js
+++ app/pages/home/style.js
@@ -1,71 +1,27 @@
 import styled from "styled-components/native";
 import { ScrollView } from "react-native";
 
-export const ViewScroll = styled(ScrollView)`
-	width: 100%;
-	height: 100%;
+export const ContainerScroll = styled(ScrollView)`
+    height: 100%;
+    width: 100%;
+    background-color: ${(props) => props.theme.colors.bg};
 `;
-export const ContainerPP = styled.View`
+
+export const Container = styled.View`
 	width: 100%;
-	height: 100%;
+	padding: 10px 20px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 15px;
 	background-color: ${(props) => props.theme.colors.bg};
 `;
 
-export const ContainerConteudo = styled.View`
-	height: 100%;
-	width: 100%;
-	align-items: center;
-`;
-
-export const ContainerConteudo1 = styled.View`
-	width: 100%;
-	height: 30%;
+export const ContainerComDoisBlocos = styled.View`
+	gap: 15px;
 	display: flex;
 	flex-direction: row;
-	justify-content: space-between;
-	padding: 15px;
-	align-items: center;
-`;
-
-export const ContainerConteudo2 = styled.View`
-	width: 100%;
-	height: 200%;
-	align-items: center;
-`;
-
-export const ButtonConteudo = styled.TouchableOpacity`
-	width: 157.5px;
-	height: 150px;
-	border-radius: 10px;
-	background: #bad9a2;
-`;
-
-export const ButtonConteudoGrande = styled.TouchableOpacity`
-	width: 330px;
-	height: 150px;
-	border-radius: 10px;
-	background: #bad9a2;
-	margin-top: 50px;
-`;
-
-export const TextoPequeno = styled.Text`
-	color: #bad9a2;
-	text-align: center;
-	font-family: "Inter-Regular";
-	font-size: 20px;
-	text-transform: uppercase;
-`;
-
-export const Title = styled.Text`
-	text-align: center;
-	font-family: "Inter-Bold";
-	font-size: 20px;
-	text-transform: uppercase;
-`;
-
-export const Div = styled.View`
-	width: 157.5px;
-	height: 150px;
-	border-radius: 10px;
-	background-color: brown;
+	justify-content: center;
+	background-color: ${(props) => props.theme.colors.bg};
 `;
